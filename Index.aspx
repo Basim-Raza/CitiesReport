610074cab76894178759e359ff92e1747a8d51de
Basim Raza <basim.raza@sofcom.net>
2021-11-16 14:33:53 +0500

    Report Generated of Table Cities - RDLC

---
+++ Index.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="CitiesReport.WebForm1" %>
+
+<%@ Register assembly="Microsoft.ReportViewer.WebForms, Version=11.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91" namespace="Microsoft.Reporting.WebForms" tagprefix="rsweb" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Cities Report</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:DropDownList ID="CountryList" runat="server">
+                <asp:ListItem>PK</asp:ListItem>
+                <asp:ListItem>IR</asp:ListItem>
+                <asp:ListItem>KSA</asp:ListItem>
+                <asp:ListItem>AUS</asp:ListItem>
+                <asp:ListItem>CN</asp:ListItem>
+            </asp:DropDownList>
+            <asp:Button ID="Button1" runat="server" BackColor="#99CCFF" Font-Bold="True" ForeColor="Black" OnClick="Button1_Click" Text="Cities" />
+
+        </div>
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+        <rsweb:ReportViewer ID="ReportViewer1" runat="server" Height="359px" Width="520px">
+        </rsweb:ReportViewer>
+    </form>
+</body>
+</html>
---
+++ Index.aspx.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CitiesReport
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var countryCode = CountryList.SelectedItem;
+
+            var connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionDB"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("select * from City where CountryCode = @countryCode", con);
+                cmd.Parameters.AddWithValue("@countryCode", countryCode.ToString());
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                ReportViewer1.LocalReport.DataSources.Clear();
+                ReportDataSource src = new ReportDataSource("DataSet1", dt);
+                ReportViewer1.LocalReport.ReportPath = "CityReport.rdlc";
+                ReportViewer1.LocalReport.DataSources.Add(src);
+            }
+        }
+    }
+}
